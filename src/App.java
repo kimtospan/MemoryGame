f8349ef75e16d80f0efb8ff9a34658d397ca628f
Kimtospan <dimitrisvas014@gmail.com>
2025-01-06 20:09:37 +0200

    first commit javaax runs

--- src/App.java
+++ src/App.java
@@ -1,6 +1,20 @@
-public class App {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class App extends Application {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Hello, World! 3");
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Memory Game");
+        Scene scene = new Scene(label, 400, 200);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("JavaFX App");
+        primaryStage.show();
     }
 }
